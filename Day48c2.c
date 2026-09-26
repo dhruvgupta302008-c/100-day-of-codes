1123dbfa3c618c9cf24d92aa6eef59189e8b9b9d
dhruvgupta302008-c <dhruv.gupta302008@gmail.com>
2026-09-26 20:46:55 +0530

    Add files via upload

---
+++ Day48c2.c
@@ -0,0 +1,43 @@
+/*
+Q96 (Strings)
+Reverse each word in a sentence without changing the word order.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[200], temp;
+    int i, start = 0, end;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; ; i++)
+    {
+        if(str[i] == ' ' || str[i] == '\n' || str[i] == '\0')
+        {
+            end = i - 1;
+
+            while(start < end)
+            {
+                temp = str[start];
+                str[start] = str[end];
+                str[end] = temp;
+
+                start++;
+                end--;
+            }
+
+            start = i + 1;
+        }
+
+        if(str[i] == '\0')
+            break;
+    }
+
+    printf("Result: %s", str);
+
+    return 0;
+}
